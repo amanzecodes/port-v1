9c0f4151d2759283e6dc61eadcb015f417ccc1c4
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-15 00:49:06 +0100

    finetunig the content of my about mme section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,29 +1,37 @@
 const About = () => {
-    return (
-      <section id="about" className="py-20 mt-20">
-        <div className="mx-auto px-4">
-          <div className="flex flex-col md:flex-col lg:flex-row w-full justify-between items-center py-10 border-t border-b border-dotted border-black/25 gap-6 md:gap-8">
-            <div>
-              <h2 className="text-xl md:text-3xl lg:text-4xl font-thin">About Me</h2>
-            </div>
-            <div className="max-w-[700px]">
-              <div className="flex flex-col gap-6">
-                <p className="text-xl md:text-2xl lg:text-4xl">
-                I study Computer Science at Covenant University with a passion for web development. I'm experienced in Next.js &amp; TypeScript, Framer Motion, and Tailwind CSS.
-                </p>
-                <p className="text-xl md:text-2xl lg:text-4xl">
-                  Apart from coding, I enjoy gaming, exploring new tech trends, and Amapiano. I am also a huge fan of Mark Zuckerberg, I wish to be better one day.
-                </p>
-                <p className="text-xl md:text-2xl lg:text-4xl">
-                  Always eager to learn, I seek opportunities for growth.
-                </p>
-              </div>
+  return (
+    <section id="about" className="py-20 mt-20">
+      <div className="mx-auto px-4">
+        <div className="flex flex-col md:flex-col lg:flex-row w-full justify-between items-center py-10 border-t border-b border-dotted border-black/25 gap-6 md:gap-8">
+          <div>
+            <h2 className="text-xl md:text-3xl lg:text-4xl font-thin">
+              About Me
+            </h2>
+          </div>
+          <div className="max-w-[700px]">
+            <div className="flex flex-col gap-6">
+              <p className="text-xl md:text-2xl lg:text-4xl">
+                I specialize in designing and developing modern, responsive
+                websites for businesses and personal brands. With expertise in
+                Next.js, TypeScript, and Tailwind CSS, I create fast, visually
+                appealing, and user-friendly web experiences that leave a
+                lasting impression.
+              </p>
+              <p className="text-xl md:text-2xl lg:text-4xl">
+                Beyond coding, I enjoy gaming, exploring emerging tech trends,
+                and listening to Amapiano. I admire Mark Zuckerberg’s impact on
+                the tech industry and strive to surpass expectations in my own
+                journey.
+              </p>
+              <p className="text-xl md:text-2xl lg:text-4xl">
+                Always eager to learn, I seek opportunities for growth.
+              </p>
             </div>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default About;
-  
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className='mx-auto px-2'>
+            <div>Available for work</div>
+            <h2>Enough Talk. Let's Make something great together.</h2>
+            <nav>
+                
+            </nav>
+        </div>
+    </footer>
   )
 }
 
