56e20ab64c6fd5dd909ff014d80293d56f652f92
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-21 00:34:36 +0100

    updated my stack layout

--- src/app/components/Stack.tsx
+++ src/app/components/Stack.tsx
@@ -1,40 +1,54 @@
-"use client";
-import { motion, useAnimate } from "framer-motion";
-import { useEffect, useState } from "react";
+import { HoverEffect } from "./ui/card-hover-effect";
 
-const techStack = ["Next JS", "TypeScript", "React JS", "Node JS", "Express JS", "MongoDB"];
-
-export default function CallToAction() {
-  const [isHovered, setIsHovered] = useState(false);
-  const [scope, animate] = useAnimate();
-
-  useEffect(() => {
-    animate("[data-animate]", { x: "-50%" }, { duration: isHovered ? 65 : 40, ease: "linear", repeat: Infinity });
-  }, [isHovered, animate]);
-
+export default function Stack() {
   return (
-    <section id="stack" className="py-16" ref={scope}>
-      <div className="flex w-full justify-center items-center py-6">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-thin">My Tech Stack</h1>
+    <section id="stack">
+      <div className="container">
+      <div className="flex items-center justify-center py-20 md:py-12 sm:py-8">
+        <h1 className="text-stone-200 text-6xl md:text-5xl sm:text-2xl">My Development Stack</h1>
       </div>
-      <div className="overflow-x-clip p-4 flex">
-        <motion.div
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          data-animate
-          className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
-        >
-          {Array(6) // Repeat the stack for continuous animation
-            .fill(techStack)
-            .flat()
-            .map((tech, index) => (
-              <div key={index} className="flex items-center gap-16">
-                <span className="text-lime-400 text-7xl">&#10038;</span>
-                <span className="group-hover:text-lime-400">{tech}</span>
-              </div>
-            ))}
-        </motion.div>
-      </div>
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={stack} />
+    </div>
+    </div>
     </section>
   );
 }
+export const stack = [
+  {
+    title: "Frontend",
+    description:
+      "Next JS, React JS, TypeScript, JavaScript.",
+    
+  },
+  {
+    title: "Backend",
+    description:
+      "Node JS—Express JS.",
+    
+  },
+  {
+    title: "Utilities",
+    description:
+      "Zustand, Clerk, Axios, Zod, Prisma.",
+    
+  },
+  {
+    title: "Database",
+    description:
+      "MongoDB, Redis, PostgreSQL.",
+    
+  },
+  {
+    title: "Styling",
+    description:
+      "Tailwind CSS, Aceternity, Shadcn, Framer Motion.",
+    
+  },
+  {
+    title: "Development Tools",
+    description:
+      "VS Code, Github, Postman, Git.",
+    
+  },
+];
