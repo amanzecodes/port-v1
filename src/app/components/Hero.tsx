13f512462ba2810f315baf48afdac72603cacaa5
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-31 21:47:16 +0100

    chage d the hero section cta button

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import SplitType from "split-type";
 import { useAnimate, motion, stagger } from "framer-motion";
 import { Spotlight } from "./ui/Spotlight";
+import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
 const Hero = () => {
   const [titleScope, titleAnimate] = useAnimate();
@@ -75,34 +76,7 @@ const Hero = () => {
             className="flex flex-col sm:flex-row items-center justify-center mt-10 gap-4 sm:gap-6"
           >
             <a href="#projects" className="mt-8">
-            <button className="bg-transparent no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
-              {" "}
-              <span className="absolute inset-0 overflow-hidden rounded-full">
-                {" "}
-                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />{" "}
-              </span>{" "}
-              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-transparent py-2 px-6 ring-1 ring-white/10 ">
-                {" "}
-                <span> View my work </span>{" "}
-                <svg
-                  fill="none"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  width="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  {" "}
-                  <path
-                    d="M10.75 8.75L14.25 12L10.75 15.25"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                  />{" "}
-                </svg>{" "}
-              </div>{" "}
-              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />{" "}
-            </button>
+            <InteractiveHoverButton>View my work</InteractiveHoverButton>;
             </a>
           </motion.div>
         </div>
